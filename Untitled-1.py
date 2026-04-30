3fed62300da10d0278e752fd36eff3381a430b25
23thirokawaberry-sudo <23thirokawaberry@wakatipu.school.nz>
2026-05-01 10:12:57 +1200

    4/30/2026

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,11 +1,9 @@
 #global variables, lists and dictionaries -------
 loop = False
-foods = {"chips": [3.99, True], "salad": [4.49, True], "chicken": [5.99, False], "pie": [8.99, False], 
-         "vegan pie": [8.49, True], "fruits": [4.99, True], "burger": [10.49, False], "fish": [6.49, False],
-         "sausage roll": [8.99, False], "stir fry veggies": [12.99, True],  "noodles": [4.49, False], "pizza": [10.99, False],
-         "bacon bits": [7.99, False], "garlic bread": [3.49, True]}
-
-# combo ideas: Chips + chicken, chips + fish, salad + fruits, pie + sausage roll, vegan pie + stir fry veggies, fruits + fish, stir fry veggies + noodles, stir fry veggies + bacon bits, burger + burger
+foods = {"chips": [3.99, 1], "salad": [4.49, 1], "chicken": [5.99, 2], "pie": [8.99, 2], 
+         "vegan pie": [8.49, 1], "fruits": [4.99, 1], "burger": [10.49, 2], "fish": [6.49, 2],
+         "sausage roll": [8.99, 2], "stir fry veggies": [12.99, 1],  "noodles": [4.49, 2], "pizza": [10.99, 2],
+         "bacon bits": [7.99, 2], "garlic bread": [3.49, 1]}
 
 #functions
 def validify_int_input(prompt, invalid):
@@ -18,45 +16,43 @@ def validify_int_input(prompt, invalid):
 
 def list_print():
     newline = 0
-    for food in foods.keys():
+    for food, details in foods.items():
         if newline == 5:
-            print(food)
+            print(f"{food} -- ${details[0]}")
             newline = 0
-        
         else:
-            print(food, end=", ")#fix this later
+            print(f"{food} -- ${details[0]}", end=" | ")#fix this later
             newline += 1
-
 
 def get_cost(food):
     """Gets the price of selected food"""
     if food in foods:
         return foods[food][0]
 
-
 def vegan_option(food):
     """Checks if the food has a vegan option/is vegan"""
     if foods[food][1] == True:
-        return "Vegan"
-    else:
-        return "No" 
+        return food
 
 def menu(cart):
+    """Single menu"""
     """Shows foods and lets user order."""
+    
     print("Here is the list of items that you can order. ")
-    loop = True
     list_print()
     """prints all available food types"""
     
+    loop = True
+    exit = ["x", "end", "cancel", "finish", "done"]
     while loop == True:
-        choice = input("Please order them indivisually. Enter X or cancel to cancel, and P or pay to pay for your meal.").lower()
-        if get_cost(choice):
-            print(choice)
-        elif choice == "x" or choice == "cancel":
+        choice = input(f"Please order them indivisually. Enter x, end, cancel, finish or done to cancel. ").lower()
+        if choice in foods:
+            cart.append(choice)
+            print(f"Ordered {choice} successfully!")
+        elif choice in exit:
             loop = False
         else:
             print("Invalid; that choice is not available or is not a valid input")
-"""Single menu"""
 
 def view(cart):
     print('temp')
@@ -65,19 +61,21 @@ def payment(cart):
     """Payment"""
     total_Cost = 0
     for items in cart:
-        print(f"Total cost for {items} is {get_cost(items)}")
+        print(f"Cost for {items} is {get_cost(items)}")
         total_Cost += get_cost(items)
+    print(f"Total cost is {total_Cost}")
 
 def main():
     cart = []
     
-    task = validify_int_input("What do you want to do? \n1. Browse menu | 2. View my order | 3. Proceed to payment", "Invalid; please use numbers")
-    if task == 1:
-        menu(cart)
-    elif task == 2:
-        view(cart)
-    elif task == 3:
-        payment(cart)
-    else:print("Invalid; please use the appropriate numbers")
+    while True:
+        task = validify_int_input("What do you want to do? \n1. Browse menu | 2. View my order | 3. Proceed to payment", "Invalid; please use numbers")
+        if task == 1:
+            menu(cart)
+        elif task == 2:
+            view(cart)
+        elif task == 3:
+            payment(cart)
+        else:print("Invalid; please use the appropriate numbers")
 
 main()
