03f746c9c19cfe67b7451edb2e88b9a3630767a9
23thirokawaberry-sudo <23thirokawaberry@wakatipu.school.nz>
2026-05-03 14:00:04 +1200

    5/3/2026 - 4

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,5 +1,6 @@
-#global variables, lists and dictionaries -------
-FOODS = {"chips": [3.99, 1], "salad": [4.49, 1], "chicken": [5.99, 1], "pie": [8.99, 2], 
+"""This program is a food ordering program."""
+# global variables, lists and dictionaries -------
+FOODS = {"chips": [3.99, 1], "salad": [4.49, 1], "chicken": [5.99, 1], "pie": [8.99, 2],
          "vegan pie": [8.49, 2], "fruits": [4.99, 1], "burger": [10.49, 2], "fish": [6.49, 1],
          "sausage roll": [8.99, 1], "stir fry veggies": [12.99, 3],  "noodles": [4.49, 1], "pizza": [10.99, 2],
          "bacon bits": [7.99, 1], "garlic bread": [3.49, 1]}
@@ -7,9 +8,9 @@ FOODS = {"chips": [3.99, 1], "salad": [4.49, 1], "chicken": [5.99, 1], "pie": [8
 EXIT = ["x", "end", "cancel", "finish", "done"]
 
 
-#functions
+# functions
 def validify_int_input(prompt, invalid):
-    """Validifies intergers"""
+    """Validifies intergers."""
     while True:
         try:
             return int(input(prompt))
@@ -18,13 +19,13 @@ def validify_int_input(prompt, invalid):
 
 
 def get_cost(food):
-    """Gets the cost of selected food"""
+    """Get the cost of selected food."""
     if food in FOODS:
         return FOODS[food][0]
-    
+
 
 def get_order_scale(cart):
-    """Gets the discount weight of the item"""
+    """Get the discount weight of the item."""
     scale = 0
     for item in cart:
         if item in FOODS:
@@ -33,7 +34,7 @@ def get_order_scale(cart):
 
 
 def menu(cart):
-    """Shows foods and lets user order."""
+    """Show foods and lets user order."""
     new_line = 0  # used for counting number of prints before a new line is created
     print("Here is the list of items that you can order. ")
     for food, details in FOODS.items():
@@ -46,8 +47,8 @@ def menu(cart):
     print("")  # stops the next print from merging with the previous print statement
 
     loop = True
-    while loop == True:
-        choice = input(f"Please order them indivisually. Enter x, end, cancel, finish or done to cancel. \n").lower()
+    while loop is True:
+        choice = input("Please order them indivisually. Enter x, end, cancel, finish or done to cancel. \n").lower()
         if choice in FOODS:
             cart.append(choice)
             print(f"Ordered {choice} successfully!")
@@ -58,7 +59,7 @@ def menu(cart):
 
 
 def view(cart):
-    """View and remove items from cart"""
+    """View and remove items from cart."""
     new_line = 0  # same as line 37
     for item in cart:
         if new_line == 8:
@@ -70,7 +71,7 @@ def view(cart):
     print("")  # same as line 46
 
     loop = True
-    while loop == True:
+    while loop is True:
         item = input("If you want to remove an item, then enter the name of the item. Otherwise enter x, end, cancel, finish or done. \n").lower()
         if item in cart:
             cart.remove(item)
@@ -81,19 +82,19 @@ def view(cart):
 
 
 def payment(cart):
-    """Payment"""
+    """Payment."""
     total_cost = 0
     for items in cart:
         total_cost += get_cost(items)
         total_cost = round(total_cost, 2)
     discount_rate = get_order_scale(cart)
-    if discount_rate > 14: 
+    if discount_rate > 14:
         total_cost = round(total_cost * 0.8, 2)
     elif discount_rate > 6:
         total_cost = round(total_cost * 0.9, 2)
 
     print(f"{len(cart)} items are in the cart and the total cost is {total_cost} with discounts applied.")
-    print(f"You can get a 10% discount for having an order size greater than 6, and a 20% discount for an order size greater than 14.")
+    print("You can get a 10% discount for having an order size greater than 6, and a 20% discount for an order size greater than 14.")
     payment_process = input("Do you want to pay and finish or return? \ny/n \n").lower()
     if payment_process == 'y':
         print("Paid successfully!")
@@ -107,9 +108,10 @@ def payment(cart):
 
 
 def main():
+    """Call other functions."""
     cart = []
     shopping = True
-    while shopping == True:
+    while shopping is True:
         task = validify_int_input("What do you want to do? \n1. Browse menu | 2. View my order | 3. Proceed to payment \n", "Invalid; please use numbers")
         if task == 1:
             menu(cart)
@@ -117,7 +119,8 @@ def main():
             view(cart)
         elif task == 3:
             shopping = payment(cart)
-        else:print("Invalid; please use the appropriate numbers")
+        else:
+            print("Invalid; please use the appropriate numbers")
 
 
 main()
